2b0404b8e4c54093738e90caef472e3c80117438
Ryan Castle <Ryan.Castle5@det.nsw.edu.au>
2026-09-05 17:49:38 +1000

    Remove HealthLink and street address, update referrer copy

--- src/data/contact.js
+++ src/data/contact.js
@@ -2,8 +2,6 @@
 export const PHONE = '0477 022 828';
 export const PHONE_TEL = '+61477022828';
 export const EMAIL = 'laura@otellaspeech.com.au';
-export const HEALTHLINK = 'OTELLASP';
-export const ADDRESS_LINE1 = '12 West Street';
 export const ADDRESS_LINE2 = 'Umina Beach NSW 2257';
 export const ABN = '43 870 391 205';
 export const HOURS = 'Tuesday to Friday, 8am–4pm';
